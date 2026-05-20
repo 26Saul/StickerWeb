9ba3c11cde98a3a942b1769f8601b79a080987bf
centrokuma <saulhgonzalez06@gmail.com>
2026-05-20 11:25:24 +0100

    2time

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/StickerWeb/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
